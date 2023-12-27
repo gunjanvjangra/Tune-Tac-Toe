46aa178e38c88fe5e59d87daba54694976996b3c
Gunjan Vaishnavi Jangra <124075942+gunjanvjangra@users.noreply.github.com>
2023-12-27 22:00:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const announcer = document.querySelector(".announcer");
 
   let board = ["", "", "", "", "", "", "", "", ""];
-  let currentPlayer = "O"; // Human player starts
+  let currentPlayer = "O";
   let isGameActive = true;
 
   const PLAYERX_WON = "PLAYERX_WON";
@@ -79,7 +79,6 @@ window.addEventListener("DOMContentLoaded", () => {
     playerDisplay.classList.add(`player${currentPlayer}`);
 
     if (currentPlayer === "X" && isGameActive) {
-      // Computer's turn
       setTimeout(() => computerTurn(), 500);
     }
   };
@@ -95,15 +94,12 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   const computerTurn = () => {
-    // Get empty tiles
     const emptyTiles = tiles.filter((tile) => tile.innerText === "");
     if (emptyTiles.length > 0) {
-      // Randomly select an empty tile for the computer's move
       const randomIndex = Math.floor(Math.random() * emptyTiles.length);
       const computerTile = emptyTiles[randomIndex];
       const computerIndex = tiles.indexOf(computerTile);
 
-      // Simulate the computer's click after a delay
       setTimeout(() => userAction(computerTile, computerIndex), 500);
     }
   };
@@ -114,7 +110,6 @@ window.addEventListener("DOMContentLoaded", () => {
     announcer.classList.add("hide");
 
     if (currentPlayer === "O") {
-      // If the last game ended with the computer's move, start with human player
       changePlayer();
     }
 
